96d47d2da432156e517ae16a38419849c57a410e
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2022-08-24 20:58:41 +0100

    roman number solution

--- src/main/java/org/bfi/tech/MathChallenge.java
+++ src/main/java/org/bfi/tech/MathChallenge.java
@@ -1,7 +1,6 @@
 package org.bfi.tech;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class MathChallenge {
     private MathChallenge() {
@@ -62,11 +61,86 @@ public class MathChallenge {
         return crc % 10;
     }
 
+    static Map<Character, Integer> romanNumberMap = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1_000
+    );
+
     public static int fromRomanToDecimal(String romanNumber) {
-        return 0;
+        int value = 0;
+        int lastDigitValue = 0;
+
+        for (int i = romanNumber.length() - 1; i >= 0; i--) {
+            final char romanDigit = romanNumber.charAt(i);
+            final int digitValue = romanNumberMap.getOrDefault(romanDigit, 0);
+
+            final boolean addMode = digitValue >= lastDigitValue;
+            if (addMode) {
+                value += digitValue;
+                lastDigitValue = digitValue;
+            } else {
+                value -= digitValue;
+            }
+        }
+        return value;
+    }
+
+    static Map<Integer, String> intToRomanMap = new TreeMap<>() {{
+        put(1, "I");
+        put(4, "IV");
+        put(5, "V");
+        put(9, "IX");
+        put(10, "X");
+        put(40, "XL");
+        put(50, "L");
+        put(90, "XC");
+        put(100, "C");
+        put(400, "CD");
+        put(500, "D");
+        put(900, "CM");
+        put(1_000, "M");
+    }};
+
+    static String repeatCharSequence(final String value, final int repetitionTimes) {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < repetitionTimes; i++) {
+            builder.append(value);
+        }
+        return builder.toString();
     }
 
     public static String fromDecimalToRoman(int decimalNumber) {
-        return null;
+        final StringBuilder result = new StringBuilder();
+        int remainder = decimalNumber;
+
+        final Comparator<Integer> reversed = Comparator.reverseOrder();
+        final Map<Integer, String> sortedIntToRomanDigit = new TreeMap<>(reversed);
+        sortedIntToRomanDigit.putAll(intToRomanMap);
+
+        var iterator = sortedIntToRomanDigit.entrySet().iterator();
+        while (iterator.hasNext() && remainder > 0) {
+            final Map.Entry<Integer, String> entry = iterator.next();
+
+            final int multiplier = entry.getKey();
+            final String romanDigit = entry.getValue();
+
+            if (remainder >= 900 && romanDigit.equals("D")) {
+                result.append("CM");
+                remainder -= 900;
+            } else if (remainder >= 4 && romanDigit.equals("I")) {
+                result.append("IV");
+                remainder -= 4;
+            } else {
+                result.append(repeatCharSequence(romanDigit, remainder / multiplier));
+                remainder = remainder % multiplier;
+            }
+        }
+
+        return result.toString();
     }
 }
