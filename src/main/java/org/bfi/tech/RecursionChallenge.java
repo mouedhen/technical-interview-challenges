ae0de7c3b3a0b7697b1f0ded476ebb621b36597c
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2022-08-25 11:56:20 +0100

    recursive is number palindrome

--- src/main/java/org/bfi/tech/RecursionChallenge.java
+++ src/main/java/org/bfi/tech/RecursionChallenge.java
@@ -9,13 +9,9 @@ public class RecursionChallenge {
     }
 
     public static String toBinary(final int input) {
-        if (input < 0) {
-            throw new IllegalArgumentException("input must be >= 0");
-        }
+        if (input < 0) throw new IllegalArgumentException("input must be >= 0");
 
-        if (input <= 1) {
-            return String.valueOf(input);
-        }
+        if (input <= 1) return String.valueOf(input);
 
         final int lastDigit = input % 2;
         final int remainder = input / 2;
@@ -23,13 +19,9 @@ public class RecursionChallenge {
     }
 
     public static String toOctal(final int input) {
-        if (input < 0) {
-            throw new IllegalArgumentException("input must be >= 0");
-        }
+        if (input < 0) throw new IllegalArgumentException("input must be >= 0");
 
-        if (input < 8) {
-            return String.valueOf(input);
-        }
+        if (input < 8) return String.valueOf(input);
 
         final int lastDigit = input % 8;
         final int remainder = input / 8;
@@ -37,9 +29,7 @@ public class RecursionChallenge {
     }
 
     static String asHexDigit(final int input) {
-        if (input < 0) {
-            throw new IllegalArgumentException("input must be >= 0");
-        }
+        if (input < 0) throw new IllegalArgumentException("input must be >= 0");
 
         if (input <= 15) {
             final char hexDigit = "0123456789ABCDEF".charAt(input);
@@ -50,13 +40,9 @@ public class RecursionChallenge {
     }
 
     public static String toHex(int input) {
-        if (input < 0) {
-            throw new IllegalArgumentException("input must be >= 0");
-        }
+        if (input < 0) throw new IllegalArgumentException("input must be >= 0");
 
-        if (input <= 15) {
-            return asHexDigit(input);
-        }
+        if (input <= 15) return asHexDigit(input);
 
         final int lastDigit = input % 16;
         final int remainder = input / 16;
@@ -69,22 +55,14 @@ public class RecursionChallenge {
     }
 
     public static long powerOf(final int number, final int power) {
-        if (power < 0) {
-            throw new IllegalArgumentException("Exponent must be >= 0");
-        }
+        if (power < 0) throw new IllegalArgumentException("Exponent must be >= 0");
 
-        if (power == 0) {
-            return 1;
-        }
+        if (power == 0) return 1;
 
-        if (power == 1) {
-            return number;
-        }
+        if (power == 1) return number;
 
         final long result = powerOf(number * number, power / 2);
-        if (power % 2 == 1) {
-            return number * result;
-        }
+        if (power % 2 == 1) return number * result;
 
         return result;
     }
@@ -98,8 +76,19 @@ public class RecursionChallenge {
         return result;
     }
 
+    static boolean isNumberPalindromeRec(final int origNumber, final int currentValue, final int remainingValue) {
+        if (origNumber == currentValue) return true;
+
+        if (remainingValue < 1) return false;
+        final int lastDigit = remainingValue % 10;
+        final int newCurrent = currentValue * 10 + lastDigit;
+        final int newRemaining = remainingValue / 10;
+
+        return isNumberPalindromeRec(origNumber, newCurrent, newRemaining);
+    }
+
     public static boolean isNumberPalindrome(int toCheck) {
-        return false;
+        return isNumberPalindromeRec(toCheck, 0, toCheck);
     }
 
     public static Set<String> retrievePermutations(String input) {
